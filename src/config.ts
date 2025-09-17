964a0892f8e6a3e8fa179252ecfe4f720e203c4a
Sagar <sagar.bhojane001@gmail.com>
2025-09-17 16:00:23 +0530

    adding the website build files

---
+++ src/config.ts
@@ -0,0 +1,108 @@
+export const siteConfig = {
+  name: "Sagar Bhojane",
+  title: "Data Scientist, Programmer and AI Enthusiast",
+  description: "Portfolio website of Sagar Bhojane",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "sagar.bhojane001@gmail.com",
+    linkedin: "https://www.linkedin.com/in/sagarbhojane/",
+    twitter: "",
+    github: "hhttps://github.com/sbhojane001",
+  },
+  aboutMe:
+    "Data whisperer turning retail, e-commerce, and supply chain chaos into crystal-clear insights. I build predictive models that don’t just forecast the future — they help save millions while sipping on cloud tech (AWS & Azure). Whether it’s taming time series, fine-tuning marketing spend, or decoding customer behavior, I thrive on translating messy data into business wins. Basically, if it involves data and impact, I’m your go-to plot twist.",
+  
+  skills: ["Python", "SQL", "Javascript", "AWS", "Azure", "Machine Learning Tinkerer", "Data Analyzer & Storyteller"],
+  
+  certifications: [
+    {
+      name: "Microsoft Certified: Fabrics Analytics Engineer Associate",
+      issuingAuthority: "Microsoft",
+      credentialId: "B72C0B3B3BA06905"
+    },
+    {
+      name: "AWS Certified Cloud Practitioner",
+      issuingAuthority: "AWS",
+      credentialId: "eea814884bf34000b8604c147ffd80df"
+    },
+    {
+      name: "Microsoft Certified: Azure AI Engineer Associate",
+      issuingAuthority: "Microsoft",
+      credentialId: "2B9F76404AADC41E"
+    },
+  ],
+  // projects: [
+  //   {
+  //     name: "AI Dev Roundup Newsletter",
+  //     description:
+  //       "One concise email. Five minutes. Every Tuesday. Essential AI news & trends, production-ready libraries, powerful AI tools, and real-world code examples",
+  //     link: "https://aidevroundup.com/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  //   {
+  //     name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+  //     description:
+  //       "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+  //     link: "https://fullstackextensions.com/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  //   {
+  //     name: "ExtensionKit",
+  //     description:
+  //       "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+  //     link: "https://extensionkit.io/?ref=devportfolio",
+  //     skills: ["React", "Node.js", "AWS"],
+  //   },
+  // ],
+  experience: [
+    {
+      company: "PwC",
+      title: "Senior Associate - Data Science & Analytics",
+      dateRange: "Aug 2024 - Present",
+      bullets: [
+        "Engineered AWS-powered IVR flows with Conversational AI (Lex, Connect, Lambda) that taught machines to handle 80%+ of calls — freeing up agents, cutting costs, and keeping customers happier.",
+        "Decoded pilot behavior patterns and optimized reserve placeme strategies, unlocking ~30% savings per bid period.",
+        "Led and coached associates, ensuring smooth, high-impact execution."
+      ],
+    },
+    {
+      company: "General Mills",
+      title: "Supply Chain Analyst - Demand Analytics",
+      dateRange: "Dec 2022 - Aug 2024",
+      bullets: [
+        "Led monthly statistical demand forecasting cycles for EUAU markets, enhancing accuracy by 3-4% through strategic initiatives like SKU realignment and demand segmentation, while tracking KPIs via the Accuracy Dashboard.",
+        "Developed a tool for Pack Size Optimization, Ingredient Replacement, and SKU Rationalization, projecting $20-22 million in potential savings and help Supply Chain Managers make informed decision."
+      ],
+    },
+    {
+      company: "Aretove Technologies",
+      title: "Data Scientist",
+      dateRange: "Nov 2020 - Dec 2022",
+      bullets: [
+        "Built a suite of predictive models for customer retention which predicts customer churn, lifetime value and customer segment classification – which led to overall improvement of 5% in repeat customers.",
+        "Built a forecasting framework for monthly demand and revenue, streamlining planning for marketing, merchandising, and finance teams, and reducing manual effort by 40%+.",
+        "Designed a marketing spend optimization strategy with predictive modeling, improving ROAS and saving ~$200K+ in monthly marketing spend.",
+        "Created GDPR and CCPA compliant data masking and validation scripts to support data governance and audit-readiness.",
+        "Built automated ETL validations and QA test harnesses to ensure data reliability across staging and production environment."
+      ],
+    },
+    {
+      company: "Global Power Source",
+      title: "Technical Sales Support Engineer",
+      dateRange: "Mar 2020 - Nov 2022",
+      bullets: [
+        "Automated MS SQL Server and Kepware configurations using AutoIT, saving 1–1.5 hours per task.",
+        "Designed and managed MS SQL Server databases for battery monitoring sites."
+      ],
+    }
+  ],
+  education: [
+    {
+      school: "Mumbai University",
+      degree: "Bachelor of Engineering in Electronics & Telecommunication",
+      dateRange: "2015 - 2019",
+      achievements: [
+      ],
+    },
+  ],
+};
